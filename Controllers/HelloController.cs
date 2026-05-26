d28d0b08d8d70a1c3c187cba6629eca6a8dc88d3
Alison Forster <alisonjoyforster@gmail.com>
2026-05-26 11:00:26 -0500

    Initialize Hello Controlller
    This controller provides endpoints to greet the user.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JJDemo.Controllers{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+    }
+}
